315360057e495b0afac2c10bbe8d480de5e0fb46
pratik goyal <goyal.pratik0304@gmail.com>
2020-09-01 12:33:47 +0530

    Updates

---
+++ precache-manifest.d8acac7e5cc21abfd1fec5c240f56080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0bdc26efa59c7dc5b8df465c5d0070",
+    "url": "/RYD-app/index.html"
+  },
+  {
+    "revision": "68a6048d3c61397206b8",
+    "url": "/RYD-app/static/css/main.72799fad.chunk.css"
+  },
+  {
+    "revision": "c139beec7b779f3b5f0a",
+    "url": "/RYD-app/static/js/2.d577c393.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RYD-app/static/js/2.d577c393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a6048d3c61397206b8",
+    "url": "/RYD-app/static/js/main.0dc06b5e.chunk.js"
+  },
+  {
+    "revision": "0a93937a5307cfdaee54",
+    "url": "/RYD-app/static/js/runtime-main.41ab31a2.js"
+  }
+]);
